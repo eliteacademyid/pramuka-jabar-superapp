395092e9e78c4b67822585d057e89bd181dda762
Fawwaz Syams <fawwazsyams04@gmail.com.com>
2026-08-01 17:46:57 +0700

    chore(config): update nama database ke db_epelatihan

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv(
-    "DATABASE_URL", "postgresql://postgres@localhost:5432/db_pramuka_jabar"
+    "DATABASE_URL", "postgresql://postgres@localhost:5432/db_epelatihan"
 )
 SECRET_KEY = os.getenv("SECRET_KEY", "super-apps-pramuka-jabar-secret")
 ALGORITHM = "HS256"
