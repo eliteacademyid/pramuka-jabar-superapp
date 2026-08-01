db4aab8c5250bfa14f62f67585129e4782dcbe99
Fawwaz Syams <fawwazsyams04@gmail.com.com>
2026-08-01 23:36:59 +0700

    feat: implement CertificateView page with printing functionality for LMS enrollments

--- frontend/src/views/lms/CertificateView.vue
+++ frontend/src/views/lms/CertificateView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import api from '../../../services/api'
+import api from '../../services/api'
 
 const route = useRoute()
 const router = useRouter()
