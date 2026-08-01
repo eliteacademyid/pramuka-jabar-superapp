14e71e934d956e127abbd11a9bf1a695ef9cb016
Fawwaz Syams <fawwazsyams04@gmail.com.com>
2026-08-01 17:57:03 +0700

    chore(config): update konfigurasi kredensial database lokal

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv(
-    "DATABASE_URL", "postgresql://postgres@localhost:5432/db_epelatihan"
+    "DATABASE_URL", "postgresql://postgres:syamstedc@localhost:5432/db_epelatihan"
 )
 SECRET_KEY = os.getenv("SECRET_KEY", "super-apps-pramuka-jabar-secret")
 ALGORITHM = "HS256"
